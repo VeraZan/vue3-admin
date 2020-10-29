001866c0061f47189f143dfc7f80c7ac8f4ffb2f
yfzan <yfzan@clustertech.com>
2020-10-29 09:33:04 +0800

    ant-design-vue@2.x安装与引入

---
+++ src/views/account/Login.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    登录
+    <a-button type="primary">
+      Primary
+    </a-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name:"Login"
+}
+</script>
+
+<style>
+
+</style>
